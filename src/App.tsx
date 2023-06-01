2782bf95435dafe7a197374c35e9467103129d21
CrisPer1988 <norberto.cp@hotmail.com>
2023-05-31 21:22:12 -0300

    probando

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
       
-        <Route element={<ProtectRoutes />}>
+      <Route element={<ProtectRoutes />}>
         <Route path='/restaurants' element={<Restaurants />}/>
       </Route>
       </Routes>
