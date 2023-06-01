92e5df17285f0141365f9a47dcdaa28ea9969bc0
CrisPer1988 <norberto.cp@hotmail.com>
2023-05-31 23:11:14 -0300

    changes footer

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -1,43 +1,49 @@
-import './styles/homefooter.css'
-import daygo from '../assets/DayGo.png';
+import "./styles/homefooter.css";
+import daygo from "../assets/DayGo.png";
 const HomeFooter = () => {
-
-
-  const pngImage: string = daygo;  
-
+  const pngImage: string = daygo;
 
   return (
-        <div className='container'>
-          <div id='container__div'>
-            <footer id='footer'> 
-          <nav id='container__nav'>
-        <ul>
-            <li><h4>Compañía</h4></li>
-            <li>Acerca de Nosotros</li>
-            <li>Testimonios</li>
-        </ul>
-        <ul>
-            <li><h4>Recursos</h4></li>
-            <li>Política de Privacidad</li>
-            <li>Términos y Condiciones</li>
-            <li>Contáctanos</li>
-        </ul>
-        
-        <section>
-            <img  id='logo__dg'   src={pngImage} alt=" png Image" />
-            <form id='form'>
-                <input  id='form__input'   placeholder='Ingresa tu correo electrónico'></input>
-                <button id='form__btn'   >Suscribirse</button>
-            </form>
-        </section>
-    </nav> 
-</footer>
-
-          </div>  
-        </div>
-      );
-    }
-    
+    // <div className="container">
+    //   <div id="container__div">
+        <footer className="container__footer">
+         
+            <ul className="items__footer">
+              <li>
+                <h4>Compañía</h4>
+              </li>
+              <li>Acerca de Nosotros</li>
+              <li>Testimonios</li>
+            </ul>
+            <ul className="items__footer">
+              <li>
+                <h4>Recursos</h4>
+              </li>
+              <li>Política de Privacidad</li>
+              <li>Términos y Condiciones</li>
+              <li>Contáctanos</li>
+            </ul>
 
 
-export default HomeFooter
+              
+              <form className="form__footer">
+              <img className="logo__dg" src={pngImage} alt=" png Image" />
+                <div className="content__input-footer">
+                <input
+                  id="form__input"
+                  placeholder="Ingresa tu email"
+                ></input>
+                <button id="form__btn">Suscribirse</button>
+
+                </div>
+               
+              </form>
+            
+         
+        </footer>
+    //   </div>
+    // </div>
+  );
+};
+
+export default HomeFooter;
